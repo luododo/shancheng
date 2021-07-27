969616ff1eca68e60f5980b9e770767e8b23eaa4
Adverse111 <adverse111@sohu.com>
2021-07-27 15:42:47 +0800

    '检索02'

--- gulimall-search/src/main/java/com/atguigu/gulimall/search/constant/EsConstant.java
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/constant/EsConstant.java
@@ -1,5 +1,5 @@
 package com.atguigu.gulimall.search.constant;
 
 public class EsConstant {
-    public static final String PRODUCT_INDEX = "product";//sku数据在es中的索引
+    public static final String PRODUCT_INDEX = "gulimall_product";//sku数据在es中的索引
 }
